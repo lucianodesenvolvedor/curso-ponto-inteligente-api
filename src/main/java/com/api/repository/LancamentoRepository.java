cc60b2b52f28e4b1fa373d2fd853dd78ea7570e9
admin <luciano.desenvolvedor@gmail.com>
2020-09-11 14:15:24 -0300

    Inclusão dos services

--- src/main/java/com/api/repository/LancamentoRepository.java
+++ src/main/java/com/api/repository/LancamentoRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -19,5 +20,5 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
 	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
 
-	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
+	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
 }
